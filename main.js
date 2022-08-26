734c7379c4686598989edaac70e0bfb398aeba9e
ogola5 <ogolaevance5@gmail.com>
2022-08-26 09:04:46 +0300

    completed assignment

--- main.js
+++ main.js
@@ -3,8 +3,24 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let heartIcons = document.querySelectorAll(".like-glyph")
+// console.log(heartIcons)
 
-
+heartIcons.forEach(item =>{
+  item.addEventListener('click',onClick)
+})
+function onClick(e){
+  mimicServerCall()
+  .then(data =>{
+    if (data && e.target.innerHTML ===EMPTY_HEART){
+      e.target.className = 'activated-heart'
+      e.target.innerHTML = FULL_HEART
+    }else if (data || e.target.innerHTML ===FULL_HEART){
+      e.target.className = 'like-glyph'
+      e.target.innerHTML = EMPTY_HEART
+    }
+  })
+}
 
 
 //------------------------------------------------------------------------------
